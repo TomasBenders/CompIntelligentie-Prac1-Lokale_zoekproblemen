13831eb9c4875eb1af40b94f1a301b0173bee4ce
Tomas Benders <t.a.benders@students.uu.nl>
2021-11-24 17:48:21 +0100

    Added parsing and visualizing

--- SudokuSolver/SudokuSolver/Program.cs
+++ SudokuSolver/SudokuSolver/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using SudokuSolver;
+using SudokuSolver.Properties;
+
 Console.WriteLine("Sudoku is solved, bicchh");
+
+List<SudokuGrid> sudokuGrids = SudokuGrid.ParsePuzzels(Resources.Sudoku_puzzels_5);
+foreach (var item in sudokuGrids)
+{
+    item.PrintGrid(0, 0);
+}
---
+++ SudokuSolver/SudokuSolver/SudokuGrid.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver
+{
+    internal class SudokuGrid
+    {
+        byte[,] gridValues = new byte[9,9];
+
+        private byte[,] FromByteArray(byte[] values)
+        {
+            byte[,] result = gridValues;
+            if (values.Length != result.Length)
+                throw new ArgumentException("array of sudoku values is not as big as the sudoku grid");
+
+            for (int x = 0; x < gridValues.GetLength(0); x++)
+                for (int y = 0; y < gridValues.GetLength(1); y++)
+                    gridValues[x, y] = values[x + y * gridValues.GetLength(0)];
+
+            return result;
+        }
+
+        internal SudokuGrid(string values)
+        {
+            var sd = values.Split();
+            var ssd = sd.Select(int.Parse).ToArray();
+            gridValues = FromByteArray(values.Split().Select(byte.Parse).ToArray());
+        }
+
+        internal void PrintGrid(int left, int top)
+        {
+            for (int x = 0; x < gridValues.GetLength(0); x++)
+                for (int y = 0; y < gridValues.GetLength(1); y++)
+                {
+                    Console.SetCursorPosition(left + x, top + y);
+                    Console.Write(gridValues[x, y]);
+                }
+        }
+
+        internal static List<SudokuGrid> ParsePuzzels(string puzzels)
+        {
+            List<SudokuGrid> result = new();
+
+            string[] lines = puzzels.Split("\n");
+            for (int i = 1; i < lines.Length; i+=2)
+                result.Add(new SudokuGrid(lines[i].Trim()));
+            return result;
+        }
+    }
+}
