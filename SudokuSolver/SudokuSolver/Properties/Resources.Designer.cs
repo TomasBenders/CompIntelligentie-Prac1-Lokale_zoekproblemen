fba51f52f85cceb08169d020e68e89cc66c2b606
Tomas Benders <t.a.benders@students.uu.nl>
2021-12-03 10:26:33 +0100

    Skeleton

--- SudokuSolver/SudokuSolver/Properties/Resources.Designer.cs
+++ SudokuSolver/SudokuSolver/Properties/Resources.Designer.cs
@@ -66,7 +66,7 @@ namespace SudokuSolver.Properties {
         ///Grid  02
         /// 2 0 0 0 8 0 3 0 0 0 6 0 0 7 0 0 8 4 0 3 0 5 0 0 2 0 9 0 0 0 1 0 5 4 0 8 0 0 0 0 0 0 0 0 0 4 0 2 7 0 6 0 0 0 3 0 1 0 0 7 0 4 0 7 2 0 0 4 0 0 6 0 0 0 4 0 1 0 0 0 3
         ///Grid  03
-        /// 0 0 0 0 0 0 9 0 7 0 0 0 4 2 0 1 8 0 0 0 0 7 0 5 0 2 6 1 0 0 9 0 4 0 0 0 0 5 0 0 0 0 0 4 0 0 0 0 5 0 7 0 0 9 9 2 0 1 0 8 0 0 0 0 3 4 0 5 9 0 0 0 5 0 7 0 0 0 0  [rest of string was truncated]&quot;;.
+        /// 0 0 0 0 0 0 9 0 7 0 0 0 4 2 0 1 8 0 0 0 0 7 0 5 0 2 6 1 0 0 9 0 4 0 0 0 0 5 0 0 0 0 0 4 0 0 0 0 5 0 7 0 0 9 9 2 0 1 0 8 0 0 0 0 3 4 0 5 9 0 0 0 5 0 7 0 0 [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string Sudoku_puzzels_5 {
             get {
